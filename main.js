fb3c8d3aa6e2f0fcf178fc5d840d07a7132d035f
thaioneves <thais.oneves2101@gmail.com>
2026-06-08 19:55:46 -0300

    adição de comandos

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+const botao = document.querySelector("#btnCalcular");
+const numero1 = document.querySelector("#num1");
+const numero2 = document.querySelector("#num2");
+const resultado = document.querySelector("#resultado");
+
+//botao.addEventListener("click", soma);
+
 function soma(){
 
-let numero1, numero2, resultado;
+let num1, num2, resCalculo;
 
-numero1 = Number(document.querySelector("#num1").value);
-numero2 = Number(document.querySelector("#num2").value);
-resultado = document.querySelector("#resultado").innerHTML;
+num1 = Number(numero1.value);
+num2 = Number(numero2.value);
 
-resultado = numero1+numero2;
-
-document.querySelector("#resultado").innerHTML = resultado;
-
+resCalculo = num1+num2;
+resultado.innerHTML = resCalculo;
 }
-
+botao.addEventListener("click", soma);
