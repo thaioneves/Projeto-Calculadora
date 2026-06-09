473c1055f70ac7b34eb1fa7013556cc0c2b89e44
thaioneves <thais.oneves2101@gmail.com>
2026-06-08 21:06:34 -0300

    Adição de comandos

--- main.js
+++ main.js
@@ -16,4 +16,23 @@ num2 = Number(numero2.value);
 resCalculo = num1+num2;
 resultado.innerHTML = resCalculo;
 }
+
+function validarEntradas(){
+    if (isNaN(numero1.value) || isNaN(numero2.value)) {
+       
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function mostrarMensagemErro(){
+     resultado.innerHTML = "Valores inválidos. Por favor, insira números válidos."
+}
+
+if (validarEntradas()==true) {
 botao.addEventListener("click", soma);
+}else {
+    mostrarMensagemErro();
+}
+
