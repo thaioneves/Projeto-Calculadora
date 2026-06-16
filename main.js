5a169cbad9be0d5dd6fe619dcdb1cd4f7a02eedb
thaioneves <thais.oneves2101@gmail.com>
2026-06-15 22:22:16 -0300

    exercicios feitos

--- main.js
+++ main.js
@@ -1,20 +1,80 @@
-const botao = document.querySelector("#btnCalcular");
+const botao = document.querySelector("#btnSomar");
+const botaoSubtracao = document.querySelector("#btnSubtrair");
+const botaoMultiplicar = document.querySelector("#btnMultiplicar");
+const botaoDivisao= document.querySelector("#btnDivisao");
+const botaoLimpar = document.querySelector("#btnLimpar");
 const numero1 = document.querySelector("#num1");
 const numero2 = document.querySelector("#num2");
 const resultado = document.querySelector("#resultado");
+const caixaResultado = document.querySelectorAll(".box")[1];
 
-//botao.addEventListener("click", soma);
 
 
 function soma(){
-
 let num1, num2, resCalculo;
 
+if(validarEntradas() === true){
 num1 = Number(numero1.value);
 num2 = Number(numero2.value);
-
 resCalculo = num1+num2;
-resultado.innerHTML = resCalculo;
+caixaResultado.classList.remove("invisible")
+exibirResultado(resCalculo);
+}else{
+    mostrarMensagemErro();
+}
+}
+
+function subtrair(){
+    let num1, num2, resCalculo;
+if (validarEntradas() === true) {
+    num1 = Number(numero1.value);
+    num2 = Number(numero2.value);
+    resCalculo = num1 - num2;
+    caixaResultado.classList.remove("invisible")
+    exibirResultado(resCalculo);
+  } else {
+    mostrarMensagemErro(); 
+  }
+}
+
+function multiplicar(){
+  let num1, num2, resCalculo;
+ 
+   if (validarEntradas() === true) {//com ambos
+    num1 = Number(numero1.value);
+    num2 = Number(numero2.value);
+    resCalculo = num1 * num2;
+    exibirResultado(resCalculo);//com parametro sem retorno
+  } else {
+    mostrarMensagemErro(); //sem parametro e sem retorno
+  }
+}
+
+function dividir(){
+  let num1, num2, resCalculo;
+ 
+   if (validarEntradas() === true) {//com ambos
+    num1 = Number(numero1.value);
+    num2 = Number(numero2.value);
+
+        if (num2 === 0) {
+            resultado.innerHTML = "Valores inválidos - Divisão por zero";
+        } else {
+            resCalculo = num1 / num2;
+
+            caixaResultado.classList.remove("invisible");
+            exibirResultado(resCalculo);
+        }
+  } else {
+    mostrarMensagemErro(); //sem parametro e sem retorno
+  }
+}
+
+function limparCampos(){
+  numero1.value = "",
+  numero2.value = "",
+  resultado.innerHTML=""
+
 }
 
 function validarEntradas(){
@@ -30,13 +90,18 @@ function mostrarMensagemErro(){
      resultado.innerHTML = "Valores inválidos. Por favor, insira números válidos."
 }
 
-if (validarEntradas()==true) {
-botao.addEventListener("click", soma);
-}else {
-    mostrarMensagemErro();
-}
-
 function exibirResultado(valor){
-    resultado.innerHTML = valor;
+    resultado.innerHTML = "Resultado: " + valor;
+
+    if (valor >=0) {
+    resultado.style.color = "lightgreen"
+  } else {
+     resultado.style.color = "red"
+  }
 }
 
+botao.addEventListener("click", soma);
+botaoSubtracao.addEventListener("click", subtrair);
+botaoLimpar.addEventListener("click", limparCampos);
+botaoMultiplicar.addEventListener("click", multiplicar);
+botaoDivisao.addEventListener("click", dividir)
